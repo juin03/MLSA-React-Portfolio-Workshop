ff9e3eda006def3101f3ccc2362d4d3d4fae9784
juin03 <117788824+juin03@users.noreply.github.com>
2024-05-18 08:53:31 +0000

    template prepare

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
+// Purpose: The App.jsx file acts as the root component of your React application.
+// It holds and organizes all other components.
+
 import styles from "./App.module.css";
 import { About } from "./Components/About/About";
 import { Contact } from "./Components/Contact/Contact";
 import { Experience } from "./Components/Experience/Experience";
 import { Hero } from "./Components/Hero/Hero";
 import { Navbar } from "./Components/Navbar/Navbar";
-import { Projects } from "./Components/Projects/Projects";
 
 import React from "react";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Navbar />
+      <Navbar/>
       <Hero />
-      <About />
-      <Experience />
-      <Projects />
-      <Contact />
     </div>
   );
 }
