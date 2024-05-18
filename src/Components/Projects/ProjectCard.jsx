d5dbf5edd31a81417bb104d435efd6a704a8fc7e
juin03 <117788824+juin03@users.noreply.github.com>
2024-05-18 11:02:30 +0000

    fine tuned template

---
+++ src/Components/Projects/ProjectCard.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./ProjectCard.module.css";
+
+export const ProjectCard = ({
+  project: { title, imageSrc, description, skills, demo, source },
+}) => {
+  return (
+    <div>
+      <h1>Halo World</h1>
+    </div>
+  );
+};
+
+ProjectCard.propTypes = {
+  project: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    skills: PropTypes.arrayOf(PropTypes.string).isRequired,
+    demo: PropTypes.string.isRequired,
+    source: PropTypes.string.isRequired,
+  }).isRequired,
+};
